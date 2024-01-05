a5856ce23228153e16a30664c84ba45dd75abf32
RMSCA <howell.rmsca@outlook.com>
2024-01-05 13:22:09 -0800

    Initial commit

---
+++ src/main/java/com/laelioa/comfortbox/Reference.java
@@ -0,0 +1,7 @@
+package com.laelioa.comfortbox;
+
+public class Reference {
+    public static final String MODID = "comfortbox";
+    public static final String NAME = "ComfortBox-remake";
+    public static final String VERSION = "1.12.2-0.1.0";
+}
